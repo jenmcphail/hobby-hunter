87f485b6b175eab82fb50c52eda6f9a74f44382a
jenmcphail <mcphail.jen@gmail.com>
2016-09-08 15:31:15 -0700

    creates user and hobby migrations, controllers, and models

---
+++ app/controllers/hobbies_controller.rb
@@ -0,0 +1,24 @@
+class HobbiesController < ApplicationController
+
+  def create
+  end
+
+  def new
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'home#home'
+  root to: 'hobbies#index'
+
+  resources :hobbies
+  resources :users
+
 end
